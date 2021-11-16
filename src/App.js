81759423c584ed34c17a0390880612e4844d7cce
MCesarczyk CNC <fadal.error@gmail.com>
2021-11-16 13:06:39 +0100

    changing primary color via input added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import './App.css';
 import { colorsCalculate } from './utils/colorsCalculate';
 import ColorPalette from './ColorPalette';
 import Input from './Input';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const App = () => {
+  const [value, setValue] = useState('');
   const [primary, setPrimary] = useState('#3282c8');
-  const palette = colorsCalculate();
+  const palette = colorsCalculate(primary);
+
+  const handleChange = useCallback(({ target }) => {
+    const payload = target.value;
+
+    setValue(payload);
+
+    payload.length === 7 && setPrimary(payload);
+  }, [value]);
+
+  useEffect(() => {
+    console.log(`Primary: ${primary}`);
+  }, [primary]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+
         Choose your primary color
         <Input
-          value={primary}
-          onChange={e => setPrimary(e.target.value)}
+          value={value}
+          onChange={handleChange}
         />
         <ColorPalette palette={palette} />
         <a
