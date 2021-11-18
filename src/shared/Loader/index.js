7750db01cd9bb1a6a7d3457617f6d6057441cc50
MCesarczyk CNC <fadal.error@gmail.com>
2021-11-18 09:43:07 +0100

    added loader as static component

--- src/shared/Loader/index.js
+++ src/shared/Loader/index.js
@@ -1,9 +1,23 @@
-import { Message } from "./styled";
+import { Message, Progress, ProgressBar, ProgressInner, ProgressLabel, ProgressOuter, Wrapper } from "./styled";
 
 const Loader = ({ message }) => (
-    <Message>
-        {message}
-    </Message>
+    <>
+        <Message>
+            {message}
+        </Message>
+        <Wrapper>
+            <Progress>
+                <ProgressOuter>
+                    <ProgressInner>
+                        <ProgressBar />
+                    </ProgressInner>
+                </ProgressOuter>
+                <ProgressLabel>
+                    30%
+                </ProgressLabel>
+            </Progress>
+        </Wrapper>
+    </>
 );
 
 export default Loader;
--- src/shared/Loader/styled.js
+++ src/shared/Loader/styled.js
@@ -4,4 +4,62 @@ export const Message = styled.h1`
     color: #fff;
     font-size: 48px;
     margin-top: 50vh;
-`;
+`
+
+export const Wrapper = styled.div`
+    color: #fff;
+    padding: 42px 24px 50px;
+    color: #000000d9;
+`
+
+export const Progress = styled.div`
+    display: inline-block;
+    position: relative;
+    width: 80%;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    color: #000000d9;
+    font-size: 14px;
+    line-height: 1.5715;
+    font-variant: tabular-nums;
+    font-feature-settings: "tnum";
+`
+
+export const ProgressOuter = styled.div`
+    display: inline-block;
+    width: 100%;
+    margin-right: calc(-2em - 8px);
+    padding-right: calc(2em + 8px);
+`
+
+export const ProgressInner = styled.div`
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    overflow: hidden;
+    vertical-align: middle;
+    background-color: #f5f5f5;
+    border-radius: 100px;
+`
+
+export const ProgressBar = styled.div`
+    width: 30%;
+    height: 8px;
+    position: relative;
+    background-color: crimson;
+    border-radius: 100px;
+    transition: all .4s cubic-bezier(.08,.82,.17,1) 0s;
+`
+
+export const ProgressLabel = styled.span`
+    display: inline-block;
+    width: 2em;
+    margin-left: 8px;
+    color: #ccc;
+    font-size: 2em;
+    line-height: 1;
+    white-space: nowrap;
+    text-align: left;
+    vertical-align: middle;
+    word-break: normal;
+  `
